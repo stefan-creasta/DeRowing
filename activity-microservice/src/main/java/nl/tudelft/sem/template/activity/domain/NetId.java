f83f0167780db5ae40b2c14ef5237ffe56a10bc2
Maarten van der Weide <m.v.vanderweide@student.tudelft.nl>
2022-12-09 11:36:05 +0100

    NetId class now implements equals, fixing a bug

--- activity-microservice/src/main/java/nl/tudelft/sem/template/activity/domain/NetId.java
+++ activity-microservice/src/main/java/nl/tudelft/sem/template/activity/domain/NetId.java
@@ -4,6 +4,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
 import java.io.Serializable;
+import java.util.Objects;
 
 @NoArgsConstructor
 @Embeddable
@@ -20,5 +21,18 @@ public class NetId implements Serializable {
 
     public void setNetId(long netId) {
         this.netId = netId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof NetId)) return false;
+        NetId netId1 = (NetId) o;
+        return netId == netId1.netId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(netId);
     }
 }
