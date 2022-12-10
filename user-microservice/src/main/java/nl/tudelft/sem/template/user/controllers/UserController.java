51dcf3385b4b6fc7cf0051f494f280aef3f71473
Rithik Appachi Senthilkumar <r.appachisenthilkumar@student.tudelft.nl>
2022-12-10 02:06:30 +0100

    basic user microservice structure (without tests yet)

---
+++ user-microservice/src/main/java/nl/tudelft/sem/template/user/controllers/UserController.java
@@ -0,0 +1,43 @@
+package nl.tudelft.sem.template.user.controllers;
+
+import nl.tudelft.sem.template.user.authentication.AuthManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello World example controller.
+ * <p>
+ * This controller shows how you can extract information from the JWT token.
+ * </p>
+ */
+@RequestMapping("/user")
+@RestController
+public class UserController {
+
+    private final transient AuthManager authManager;
+
+    /**
+     * Instantiates a new controller.
+     *
+     * @param authManager Spring Security component used to authenticate and authorize the user
+     */
+    @Autowired
+    public UserController(AuthManager authManager) {
+        this.authManager = authManager;
+    }
+
+    /**
+     * Gets user by id.
+     *
+     * @return the user found in the database with the given id
+     */
+    @GetMapping("/hello")
+    public ResponseEntity<String> helloWorld() {
+        return ResponseEntity.ok("Hello " + authManager.getNetId());
+
+    }
+
+}
