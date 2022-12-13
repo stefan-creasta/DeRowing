c5b05f3503b05ef1d336f785ee7a6b243df580bf
Maarten van der Weide <m.v.vanderweide@student.tudelft.nl>
2022-12-13 13:50:23 +0100

    Removed dependency and added Gradle integration

--- boat-microservice/src/main/java/nl/tudelft/sem/template/boat/domain/Boat.java
+++ boat-microservice/src/main/java/nl/tudelft/sem/template/boat/domain/Boat.java
@@ -12,15 +12,15 @@ import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import nl.tudelft.sem.template.user.domain.User;
 
 @Entity
 @Table(name = "boats")
-@Getter @Setter @NoArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
 public class Boat {
     /**
      * Identifier for the boat.
@@ -34,9 +34,9 @@ public class Boat {
 
     // TODO: create connection to Users table
     @OneToMany(fetch = FetchType.EAGER, cascade = { CascadeType.PERSIST, CascadeType.MERGE }, mappedBy = "users")
-    private Map<Position, List<User>> rowers;
-
+    private Map<Position, List<Rower>> rowers;
     private HashMap<Position, Integer> requiredRowers;
+
     /**
      * Create new boat.
      *
@@ -61,21 +61,22 @@ public class Boat {
 
     }
 
-    public Map<Position, List<User>> getRowers() {
+    public Map<Position, List<Rower>> getRowers() {
         return this.rowers;
     }
 
     public HashMap<Position, Integer> getRequiredRowers() {
         return this.requiredRowers;
     }
+
     /**
      * Adds rower to boat.
      *
-     * @param user the user to be added
+     * @param rower the user to be added
      */
-    public void addRowerToPosition(Position position, User user) {
+    public void addRowerToPosition(Position position, Rower rower) {
         if (this.rowers.get(position).size() < this.requiredRowers.get(position)) {
-            this.rowers.get(position).add(user);
+            this.rowers.get(position).add(rower);
             int val = requiredRowers.get(position);
             requiredRowers.replace(position, val - 1);
         }
@@ -95,41 +96,31 @@ public class Boat {
     /**
      * Removes rower from the boat.
      *
-     * @param user the user to be removed
+     * @param rower the user to be removed
      * @return true, if the user was successfully removed, false otherwise
      */
-    public boolean removeRower(User user) {
+    public boolean removeRower(Rower rower) {
         // find the key that the User is mapped to
-        int pos = -1;
-        for(Map.Entry<Position, List<User>> a : rowers.entrySet()) {
-            if(a.getValue().contains(user)) {
-                a.getValue().remove(user);
+        for (Map.Entry<Position, List<Rower>> a : rowers.entrySet()) {
+            if (a.getValue().contains(rower)) {
+                a.getValue().remove(rower);
                 int val = requiredRowers.get(a.getKey());
                 requiredRowers.replace(a.getKey(), val + 1);
                 return true;
             }
         }
         return false;
-        /**rowers.forEach((key, value) -> {
-            if (value.contains(user)) {
-               value.remove(user);
-               int val = requiredRowers.get(key);
-               requiredRowers.replace(key, val + 1);
-               return true;
-            }
-        });
-
-        return false;**/
     }
 
     /**
-     * Checks whether the user is eligible to be a rower on the boat
+     * Checks whether the user is eligible to be a rower on the boat.
+
      * @param type the user's certificate
      * @param position the user's position
      * @return true whether the user is eligible, false otherwise
      */
     public boolean canRowerBeAdded(Type type, Position position) {
-        if(type.value < this.type.value) {
+        if (type.value < this.type.value) {
             return false;
         }
         return (requiredRowers.get(position) > 0);
@@ -143,11 +134,16 @@ public class Boat {
      */
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Boat)) return false;
-        Boat boat = (Boat) o;
-        if(!(id == boat.id && type == boat.type))
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Boat)) {
             return false;
+        }
+        Boat boat = (Boat) o;
+        if (!(id == boat.id && type == boat.type)) {
+            return false;
+        }
         return Objects.equals(rowers, boat.rowers) && Objects.equals(requiredRowers, boat.requiredRowers);
     }
 
