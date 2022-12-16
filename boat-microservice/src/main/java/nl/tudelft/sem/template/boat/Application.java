a90c07c22089ae811dbc49d3b452eec5fde62a5c
Viet Luong Hoang <LuongHoangViet@student.tudelft.nl>
2022-12-16 22:52:53 +0100

    Fix the bug - the BoatRepository reference
    Without this annotation, the program can not find the BoatRepository directory

--- boat-microservice/src/main/java/nl/tudelft/sem/template/boat/Application.java
+++ boat-microservice/src/main/java/nl/tudelft/sem/template/boat/Application.java
@@ -2,11 +2,13 @@ package nl.tudelft.sem.template.boat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Boat microservice application.
  */
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "nl.tudelft.sem.template.boat.repositories")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
