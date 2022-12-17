fbbda9dc0fc5b1e9c4294e33871597c8e87c71e6
Stefan Creasta <Creasta@student.tudelft.nl>
2022-12-17 16:32:21 +0100

    Draft

--- boat-microservice/src/main/java/nl/tudelft/sem/template/boat/Application.java
+++ boat-microservice/src/main/java/nl/tudelft/sem/template/boat/Application.java
@@ -2,7 +2,6 @@ package nl.tudelft.sem.template.boat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Boat microservice application.
--- boat-microservice/src/main/java/nl/tudelft/sem/template/boat/config/H2Config.java
+++ boat-microservice/src/main/java/nl/tudelft/sem/template/boat/config/H2Config.java
@@ -41,7 +41,6 @@ public class H2Config {
         dataSource.setUrl(environment.getProperty("jdbc.url"));
         dataSource.setUsername(environment.getProperty("jdbc.user"));
         dataSource.setPassword(environment.getProperty("jdbc.pass"));
-
         return dataSource;
     }
 }
