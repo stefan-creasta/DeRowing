00d7834ce5c2b959326484761a1fd5bd9579dc32
Yongcheng Huang <Y.Huang-51@student.tudelft.nl>
2022-12-19 01:55:09 +0100

    Create tests for repositories

---
+++ activity-microservice/src/test/java/nl/tudelft/sem/template/activity/domain/repositories/CompetitionRepositoryTest.java
@@ -0,0 +1,169 @@
+package nl.tudelft.sem.template.activity.domain.repositories;
+
+import nl.tudelft.sem.template.activity.domain.GenderConstraint;
+import nl.tudelft.sem.template.activity.domain.NetId;
+import nl.tudelft.sem.template.activity.domain.entities.Competition;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+import java.util.Optional;
+
+class CompetitionRepositoryTest {
+
+    private CompetitionRepository competitionRepository;
+
+    private Competition competition;
+
+    @BeforeEach
+    public void setup() {
+        competition = new Competition(new NetId("123"), "testname", 123L, 123L,
+                false, GenderConstraint.NO_CONSTRAINT, false);
+        competitionRepository = new CompetitionRepository() {
+            @Override
+            public Competition findByNetId(NetId netId) {
+                return competition;
+            }
+
+            @Override
+            public boolean existsByNetId(NetId netId) {
+                return false;
+            }
+
+            @Override
+            public List<Competition> findAll() {
+                return null;
+            }
+
+            @Override
+            public List<Competition> findAll(Sort sort) {
+                return null;
+            }
+
+            @Override
+            public List<Competition> findAllById(Iterable<NetId> netIds) {
+                return null;
+            }
+
+            @Override
+            public <S extends Competition> List<S> saveAll(Iterable<S> entities) {
+                return null;
+            }
+
+            @Override
+            public void flush() {
+
+            }
+
+            @Override
+            public <S extends Competition> S saveAndFlush(S entity) {
+                return null;
+            }
+
+            @Override
+            public void deleteInBatch(Iterable<Competition> entities) {
+
+            }
+
+            @Override
+            public void deleteAllInBatch() {
+
+            }
+
+            @Override
+            public Competition getOne(NetId netId) {
+                return null;
+            }
+
+            @Override
+            public <S extends Competition> List<S> findAll(Example<S> example) {
+                return null;
+            }
+
+            @Override
+            public <S extends Competition> List<S> findAll(Example<S> example, Sort sort) {
+                return null;
+            }
+
+            @Override
+            public Page<Competition> findAll(Pageable pageable) {
+                return null;
+            }
+
+            @Override
+            public <S extends Competition> S save(S entity) {
+                return null;
+            }
+
+            @Override
+            public Optional<Competition> findById(NetId netId) {
+                return Optional.empty();
+            }
+
+            @Override
+            public boolean existsById(NetId netId) {
+                return false;
+            }
+
+            @Override
+            public long count() {
+                return 0;
+            }
+
+            @Override
+            public void deleteById(NetId netId) {
+
+            }
+
+            @Override
+            public void delete(Competition entity) {
+
+            }
+
+            @Override
+            public void deleteAll(Iterable<? extends Competition> entities) {
+
+            }
+
+            @Override
+            public void deleteAll() {
+
+            }
+
+            @Override
+            public <S extends Competition> Optional<S> findOne(Example<S> example) {
+                return Optional.empty();
+            }
+
+            @Override
+            public <S extends Competition> Page<S> findAll(Example<S> example, Pageable pageable) {
+                return null;
+            }
+
+            @Override
+            public <S extends Competition> long count(Example<S> example) {
+                return 0;
+            }
+
+            @Override
+            public <S extends Competition> boolean exists(Example<S> example) {
+                return false;
+            }
+        };
+    }
+
+    @Test
+    void findByNetId() {
+        Assertions.assertEquals(competition, competitionRepository.findByNetId(new NetId("123")));
+    }
+
+    @Test
+    void existsByNetId() {
+        Assertions.assertFalse(competitionRepository.existsByNetId(new NetId("123")));
+    }
+}
---
+++ activity-microservice/src/test/java/nl/tudelft/sem/template/activity/domain/repositories/TrainingRepositoryTest.java
@@ -0,0 +1,169 @@
+package nl.tudelft.sem.template.activity.domain.repositories;
+
+import nl.tudelft.sem.template.activity.domain.GenderConstraint;
+import nl.tudelft.sem.template.activity.domain.NetId;
+import nl.tudelft.sem.template.activity.domain.entities.Competition;
+import nl.tudelft.sem.template.activity.domain.entities.Training;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+import java.util.Optional;
+
+class TrainingRepositoryTest {
+
+    private TrainingRepository trainingRepository;
+
+    private Training training;
+
+    @BeforeEach
+    public void setup() {
+        training = new Training(new NetId("123"), "testname", 123L, 123L);
+        trainingRepository = new TrainingRepository() {
+            @Override
+            public Training findByNetId(NetId netId) {
+                return training;
+            }
+
+            @Override
+            public boolean existsByNetId(NetId netId) {
+                return false;
+            }
+
+            @Override
+            public List<Training> findAll() {
+                return null;
+            }
+
+            @Override
+            public List<Training> findAll(Sort sort) {
+                return null;
+            }
+
+            @Override
+            public List<Training> findAllById(Iterable<NetId> netIds) {
+                return null;
+            }
+
+            @Override
+            public <S extends Training> List<S> saveAll(Iterable<S> entities) {
+                return null;
+            }
+
+            @Override
+            public void flush() {
+
+            }
+
+            @Override
+            public <S extends Training> S saveAndFlush(S entity) {
+                return null;
+            }
+
+            @Override
+            public void deleteInBatch(Iterable<Training> entities) {
+
+            }
+
+            @Override
+            public void deleteAllInBatch() {
+
+            }
+
+            @Override
+            public Training getOne(NetId netId) {
+                return null;
+            }
+
+            @Override
+            public <S extends Training> List<S> findAll(Example<S> example) {
+                return null;
+            }
+
+            @Override
+            public <S extends Training> List<S> findAll(Example<S> example, Sort sort) {
+                return null;
+            }
+
+            @Override
+            public Page<Training> findAll(Pageable pageable) {
+                return null;
+            }
+
+            @Override
+            public <S extends Training> S save(S entity) {
+                return null;
+            }
+
+            @Override
+            public Optional<Training> findById(NetId netId) {
+                return Optional.empty();
+            }
+
+            @Override
+            public boolean existsById(NetId netId) {
+                return false;
+            }
+
+            @Override
+            public long count() {
+                return 0;
+            }
+
+            @Override
+            public void deleteById(NetId netId) {
+
+            }
+
+            @Override
+            public void delete(Training entity) {
+
+            }
+
+            @Override
+            public void deleteAll(Iterable<? extends Training> entities) {
+
+            }
+
+            @Override
+            public void deleteAll() {
+
+            }
+
+            @Override
+            public <S extends Training> Optional<S> findOne(Example<S> example) {
+                return Optional.empty();
+            }
+
+            @Override
+            public <S extends Training> Page<S> findAll(Example<S> example, Pageable pageable) {
+                return null;
+            }
+
+            @Override
+            public <S extends Training> long count(Example<S> example) {
+                return 0;
+            }
+
+            @Override
+            public <S extends Training> boolean exists(Example<S> example) {
+                return false;
+            }
+        };
+    }
+
+    @Test
+    void findByNetId() {
+        Assertions.assertEquals(training, trainingRepository.findByNetId(new NetId("123")));
+    }
+
+    @Test
+    void existsByNetId() {
+        Assertions.assertFalse(trainingRepository.existsByNetId(new NetId("123")));
+    }
+}
