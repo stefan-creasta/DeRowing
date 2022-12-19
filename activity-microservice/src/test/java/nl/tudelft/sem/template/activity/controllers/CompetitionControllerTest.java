6f8c65c09b674e340fb165669b2c1918308a01ce
Yongcheng Huang <Y.Huang-51@student.tudelft.nl>
2022-12-19 02:32:46 +0100

    Create test for netId and fix competition controller test

--- activity-microservice/src/test/java/nl/tudelft/sem/template/activity/controllers/CompetitionControllerTest.java
+++ activity-microservice/src/test/java/nl/tudelft/sem/template/activity/controllers/CompetitionControllerTest.java
@@ -1,5 +1,9 @@
 package nl.tudelft.sem.template.activity.controllers;
 
+import nl.tudelft.sem.template.activity.authentication.AuthManager;
+import nl.tudelft.sem.template.activity.domain.services.CompetitionService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -8,6 +12,26 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ExtendWith(SpringExtension.class)
 class CompetitionControllerTest {
 
-    private CompetitionController sut;
+    private CompetitionController competitionController;
 
+    private AuthManager authManager;
+
+    private CompetitionService competitionService;
+
+    @BeforeEach
+    public void setup() {
+        
+    }
+
+    @Test
+    void helloWorld() {
+    }
+
+    @Test
+    void createCompetition() {
+    }
+
+    @Test
+    void findCompetitions() {
+    }
 }
