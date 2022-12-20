3aaa619a20ed8d2e6dabf1fcff81c47ddfa3dba6
Yongcheng Huang <Y.Huang-51@student.tudelft.nl>
2022-12-20 12:54:46 +0100

    Make test for CompetitionController

--- activity-microservice/src/test/java/nl/tudelft/sem/template/activity/controllers/CompetitionControllerTest.java
+++ activity-microservice/src/test/java/nl/tudelft/sem/template/activity/controllers/CompetitionControllerTest.java
@@ -1,17 +1,26 @@
 package nl.tudelft.sem.template.activity.controllers;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import io.jsonwebtoken.lang.Assert;
 import nl.tudelft.sem.template.activity.authentication.AuthManager;
+import nl.tudelft.sem.template.activity.domain.GenderConstraint;
+import nl.tudelft.sem.template.activity.domain.NetId;
+import nl.tudelft.sem.template.activity.domain.entities.Competition;
 import nl.tudelft.sem.template.activity.domain.repositories.CompetitionRepository;
 import nl.tudelft.sem.template.activity.domain.services.CompetitionService;
+import nl.tudelft.sem.template.activity.models.CompetitionCreateModel;
+import nl.tudelft.sem.template.activity.models.CompetitionFindModel;
+import nl.tudelft.sem.template.activity.profiles.MockAuthenticationManagerProfile;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -28,26 +37,51 @@ class CompetitionControllerTest {
     @Mock
     private CompetitionRepository competitionRepository;
 
+    private CompetitionCreateModel competitionCreateModel;
+
+    private Competition competition;
+
+    private CompetitionFindModel competitionFindModel;
+
     @BeforeEach
     public void setup() {
-        authManager = mock(AuthManager.class);
+        this.authManager = mock(AuthManager.class);
         competitionRepository = mock(CompetitionRepository.class);
         competitionService = new CompetitionService(competitionRepository);
         competitionController = new CompetitionController(authManager, competitionService);
+        competitionCreateModel = new CompetitionCreateModel("test", GenderConstraint.ONLY_MALE,
+                123L, false, false, 123L);
+        competitionFindModel = new CompetitionFindModel(new NetId("123"));
+        competition = new Competition(new NetId("123"), competitionCreateModel.getCompetitionName(),
+                competitionCreateModel.getBoatId(), competitionCreateModel.getStartTime(),
+                competitionCreateModel.isAllowAmateurs(), competitionCreateModel.getGenderConstraint(),
+                competitionCreateModel.isSingleOrganization());
     }
 
     @Test
     void helloWorld() {
         when(authManager.getNetId()).thenReturn("123");
-        when(competitionController.helloWorld()).thenReturn(ResponseEntity.ok("Hello 123"));
-        Assertions.assertEquals(ResponseEntity.ok("Hello 123"), competitionController.helloWorld());
+        Assertions.assertEquals(new ResponseEntity<>("Hello 123", HttpStatus.valueOf(200))
+                , competitionController.helloWorld());
     }
 
     @Test
-    void createCompetition() {
+    void createCompetition() throws Exception {
+
+        when(authManager.getNetId()).thenReturn("123");
+        when(competitionService.createCompetition(competitionCreateModel, any())).thenReturn(competition);
+
+        Assertions.assertEquals(new ResponseEntity<>("Done! The competition test is created by 123"
+                        , HttpStatus.valueOf(200))
+                , competitionController.createCompetition(competitionCreateModel));
     }
 
     @Test
-    void findCompetitions() {
+    void findCompetitions() throws Exception {
+        when(authManager.getNetId()).thenReturn("123");
+        when(competitionService.findCompetitions(any())).thenReturn(competition);
+        Assertions.assertEquals(new ResponseEntity<>("The competition created by 123 is found. " +
+                "Here is the competition: " + competition.toString(), HttpStatus.valueOf(200))
+                , competitionController.findCompetitions(competitionFindModel));
     }
 }
