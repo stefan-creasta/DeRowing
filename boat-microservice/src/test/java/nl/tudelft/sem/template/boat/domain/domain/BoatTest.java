1c16d696f9a25208712dfa9652f8403a1e85cf09
Stefan Creasta <Creasta@student.tudelft.nl>
2022-12-22 21:20:54 +0100

    Adding unit tests here
    Adding unit tests here

--- boat-microservice/src/test/java/nl/tudelft/sem/template/boat/domain/domain/BoatTest.java
+++ boat-microservice/src/test/java/nl/tudelft/sem/template/boat/domain/domain/BoatTest.java
@@ -2,6 +2,7 @@ package nl.tudelft.sem.template.boat.domain.domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
@@ -12,6 +13,8 @@ import nl.tudelft.sem.template.boat.builders.Director;
 import nl.tudelft.sem.template.boat.domain.Boat;
 import nl.tudelft.sem.template.boat.domain.NetId;
 import nl.tudelft.sem.template.boat.domain.Position;
+import nl.tudelft.sem.template.boat.domain.RequiredRowers;
+import nl.tudelft.sem.template.boat.domain.Rowers;
 import nl.tudelft.sem.template.boat.domain.Type;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -20,9 +23,60 @@ class BoatTest {
 
     private transient Director director;
 
+    private NetId coxId;
+    private NetId coachId;
+    private NetId portId1;
+    private NetId portId2;
+    private NetId starboardId1;
+    private NetId starboardId2;
+    private NetId scullingId;
+
+    private Type type;
+    private Rowers rowers;
+    private RequiredRowers requiredRowers;
+
     @BeforeEach
     public void setup() {
+        type = Type.PLUS4;
+
+        coxId = new NetId("cox");
+        coachId = new NetId("coach");
+        portId1 = new NetId("port1");
+        portId2 = new NetId("port2");
+        starboardId1 = new NetId("starboard1");
+        starboardId2 = new NetId("starboard2");
+        scullingId = new NetId("sculling");
+
+        Map<Position, List<NetId>> currentRowers = new HashMap<>();
+        currentRowers.put(Position.COX, new ArrayList<>(List.of(coxId)));
+        currentRowers.put(Position.COACH, new ArrayList<>(List.of(coachId)));
+        currentRowers.put(Position.PORT, new ArrayList<>(List.of(portId1, portId2)));
+        currentRowers.put(Position.STARBOARD, new ArrayList<>(List.of(starboardId1, starboardId2)));
+        currentRowers.put(Position.SCULLING, new ArrayList<>(List.of(scullingId)));
+        rowers = new Rowers((HashMap<Position, List<NetId>>) currentRowers);
+
+        Map<Position, Integer> amountOfPositions = new HashMap<>();
+        amountOfPositions.put(Position.COX, 1);
+        amountOfPositions.put(Position.COACH, 1);
+        amountOfPositions.put(Position.PORT, 2);
+        amountOfPositions.put(Position.STARBOARD, 2);
+        amountOfPositions.put(Position.SCULLING, 0);
+        requiredRowers = new RequiredRowers((HashMap<Position, Integer>) amountOfPositions);
+
         director = new Director();
+    }
+
+    @Test
+    void getTypeTest() {
+        Boat boat = director.constructBoat(Type.PLUS4);
+        assertEquals(Type.PLUS4, boat.getType());
+    }
+
+    @Test
+    void setTypeTest() {
+        Boat boat = director.constructBoat(Type.PLUS4);
+        boat.setType(Type.C4);
+        assertEquals(Type.C4, boat.getType());
     }
 
     @Test
@@ -86,5 +140,26 @@ class BoatTest {
     void canAddRower() {
         Boat boat = director.constructBoat(Type.PLUS8);
         assertTrue(boat.canRowerBeAdded(Position.COX));
+    }
+
+    @Test
+    void equalsSameObjectTest() {
+        Boat boat = director.constructBoat(Type.PLUS4);
+        Boat boat1 = director.constructBoat(Type.PLUS4);
+        assertEquals(boat1, boat);
+    }
+
+    @Test
+    void equalsNotBoatTest() {
+        Object other = new Boat(Type.PLUS4);
+        Boat boat = director.constructBoat(Type.PLUS4);
+        assertNotEquals(other, boat);
+    }
+
+    @Test
+    void equalsDifferentValueTest() {
+        Boat boat = director.constructBoat(Type.C4);
+        Boat boat1 = director.constructBoat(Type.PLUS4);
+        assertNotEquals(boat1, boat);
     }
 }
